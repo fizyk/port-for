a533dfcadb5072c84fa882b4ef892e29372e88ef
Grzegorz Śliwiński <fizyk@fizyk.dev>
2025-08-20 00:15:21 +0200

    Remove global variable in cli

--- port_for/cmd.py
+++ port_for/cmd.py
@@ -27,39 +27,39 @@ import sys
 from typing import Optional
 
 import port_for
+from port_for import PortStore
 from port_for.docopt import docopt
 
-store = port_for.PortStore()
 
-
-def _list() -> None:
+def _list(store: PortStore) -> None:
     for app, port in store.bound_ports():
         sys.stdout.write("%s: %s\n" % (app, port))
 
 
-def _bind(app: str, port: Optional[str] = None) -> None:
+def _bind(store: PortStore, app: str, port: Optional[str] = None) -> None:
     bound_port = store.bind_port(app, port)
     sys.stdout.write("%s\n" % bound_port)
 
 
-def _unbind(app: str) -> None:
+def _unbind(store: PortStore, app: str) -> None:
     store.unbind_port(app)
 
 
 def main() -> None:
     """port-for executable entrypoint."""
+    store = PortStore()
     args = docopt(
         __doc__,
         version="port-for %s" % port_for.__version__,
     )  # type: ignore[no-untyped-call]
     if args["<NAME>"]:
-        _bind(args["<NAME>"], args["--port"])
+        _bind(store, args["<NAME>"], args["--port"])
     elif args["--bind"]:
-        _bind(args["--bind"], args["--port"])
+        _bind(store, args["--bind"], args["--port"])
     elif args["--list"]:
-        _list()
+        _list(store)
     elif args["--unbind"]:
-        _unbind(args["--unbind"])
+        _unbind(store, args["--unbind"])
 
 
 if __name__ == "__main__":
